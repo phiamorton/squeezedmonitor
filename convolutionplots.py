42e72532600d65cb25da545f3de5b8e25c5327c1
Phia Morton <phia@Phias-MacBook-Pro-2.local>
2026-01-21 21:36:10 -0800

    added squeezing angle and there is definitely something wrong with that

--- convolutionplots.py
+++ convolutionplots.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import cmath
 
 # ----------------------------
 # Parameters
@@ -8,12 +9,12 @@ A = 1.0
 lambda_nm = 800.0      # nm
 
 num_to_avg = 10
-dA_noise = 0.1
-dphi_noise = 0.1
+dA_noise = 0.01
+dphi_noise = dA_noise
 
 dx_beam = 100.0  # nm
 N_beam = 1000.0
-
+     
 # coordinate
 x = np.linspace(-lambda_nm, lambda_nm, 2000)
 dx = x[1] - x[0]
@@ -33,65 +34,11 @@ def gaussian_beam(x, N, dx):
 beam = gaussian_beam(x, N_beam, dx_beam)
 
 # ============================================================
-# Spatial averages: (dA + dphi) vs (dA only)
-# ============================================================
-"""
-phi0 = 0.0
-
-fringe_avg_both = np.zeros_like(x)
-fringe_avg_A    = np.zeros_like(x)
-
-for _ in range(num_to_avg):
-    # both noises
-    dA   = np.random.uniform(-dA_noise, dA_noise)
-    dphi = np.random.uniform(-dphi_noise, dphi_noise)
-    fringe_avg_both += laser_fringe(x, A + dA, lambda_nm, phi0 + dphi)
-
-    # amplitude only
-    dA = np.random.uniform(-dA_noise, dA_noise)
-    fringe_avg_A += laser_fringe(x, A + np.sqrt(dA**2+dphi**2), lambda_nm, phi0)
-
-fringe_avg_both /= num_to_avg
-fringe_avg_A    /= num_to_avg
-
-signal_both = np.convolve(fringe_avg_both, beam,mode="same")
-signal_A    = np.convolve(fringe_avg_A, beam,mode="same")
-
-# ----------------------------
-# Plot spatial comparison
-# ----------------------------
-plt.figure(figsize=(8,5))
-#plt.plot(x, fringe_avg_both, label="⟨Fringe⟩ (dA + dφ)")
-#plt.plot(x, fringe_avg_A, '--', label="⟨Fringe⟩")
-#plt.plot(x, signal_both, label="Not Squeezed signal", lw=2)
-#plt.plot(x, signal_A, '--', label="Squeezed Signal", lw=2)
-#plt.plot(x, gaussian_beam(x, N_beam, 100), label="100 nm beam")
-#plt.plot(x, gaussian_beam(x, N_beam, 1), label="1 nm beam")
-size1=1
-size2=100
-size3=10
-plt.plot(x, np.convolve(fringe_avg_A, gaussian_beam(x, N_beam, size1),mode="same"), label=f"{size1} nm beam, squeezed")
-plt.plot(x, np.convolve(fringe_avg_both, gaussian_beam(x, N_beam, size1),mode="same"), label=f"{size1} nm beam, not squeezed")
-plt.plot(x, np.convolve(fringe_avg_A, gaussian_beam(x, N_beam, size2),mode="same"), label=f"{size2} nm beam, squeezed")
-plt.plot(x, np.convolve(fringe_avg_both, gaussian_beam(x, N_beam, size2),mode="same"), label=f"{size2} nm beam, not squeezed")
-plt.plot(x, np.convolve(fringe_avg_A, gaussian_beam(x, N_beam, size3),mode="same"), label=f"{size3} nm beam, squeezed")
-plt.plot(x, np.convolve(fringe_avg_both, gaussian_beam(x, N_beam, size3),mode="same"), label=f"{size3} nm beam, not squeezed")
-plt.xlabel("x [nm]")
-plt.ylabel("Intensity of Compton Scattered Photons (arb.)")
-plt.legend()
-plt.title(f'Detected photons at the fringe minima averaged over {num_to_avg} shots')
-plt.xlim([-lambda_nm/8,lambda_nm/8])
-plt.ylim([0,15])
-plt.tight_layout()
-#plt.show()
-"""
-# ============================================================
 # Phase scan with averaging
 # ============================================================
 
-num_phase = 100
+num_phase = 1000
 num_avg   = 50
-squeezing_parameter=np.exp(-1)
 phi_scan = np.linspace(0, 6.28, num_phase)
 
 mean_both = np.zeros_like(phi_scan)
@@ -114,7 +61,8 @@ def phiscan(beamsize):
         signals_both = []
         signals_A    = []
         signals_p    = []
-
+        ###HERE NEED TO FIGURE OUT SQUEEZING ANGLE
+        squeezing_parameter=np.exp((-1))*np.sin(phi-np.pi/2) # here is the amount of squeezing and phase for squeezing quadrature 
         for _ in range(num_avg):
             laser_fringe_weight=laser_fringe(x,A,lambda_nm, phi)
             dA   = np.random.normal(0, dA_noise)
@@ -139,7 +87,6 @@ def phiscan(beamsize):
             signals_p.append(np.trapezoid(signal_x, x))
             #print((A+dA*squeezing_parameter)*(phi+dphi/squeezing_parameter))
             #signals_p.append(fringe)
-            
 
         mean_both[i] = np.mean(signals_both)
         std_both[i]  = np.std(signals_both)
@@ -158,28 +105,40 @@ def phiscan(beamsize):
 def plotphiscan(beamsize): 
     mean_both1, std_both1, mean_A1, std_A1, mean_p, std_p=phiscan(beamsize)
     
-    plt.plot(phi_scan, mean_A1, lw=2, label=f"with phase squeezing, {beamsize} nm beam")
+    plt.plot(phi_scan, mean_A1, lw=2, label=f"with phase dependent squeezing, {beamsize} nm beam")
     plt.fill_between(
         phi_scan, mean_A1 - std_A1, mean_A1 + std_A1, alpha=0.3
     )
-
+    """
     plt.plot(phi_scan, mean_p, lw=2, label=f"with amplitude squeezing, {beamsize} nm beam")
     plt.fill_between(
         phi_scan, mean_p - std_p, mean_p + std_p, alpha=0.3
     )
     #print(std_both1[500], std_A1[500])
-    
+    """
     plt.plot(phi_scan, mean_both1, lw=2, label=f"without squeezing, {beamsize} nm beam")
     plt.fill_between(
         phi_scan, mean_both1 - std_both1, mean_both1 + std_both1, alpha=0.3
     )
 
+def ideal_fringe(beamsize):
+    beam = gaussian_beam(x, N_beam, beamsize)
+    signal = []
+    for phi in enumerate(phi_scan):
+        ideal_fringe=laser_fringe(x,A,lambda_nm, phi[1])
+        signal_x = ideal_fringe*beam 
+        signal.append(np.trapezoid(signal_x, x))
+    plt.plot(phi_scan,signal, label='fringe pattern with 0 error',color='black')
+    
+
 plt.figure(figsize=(7,4))
 beamsize1=10
 plotphiscan(beamsize1)
+ideal_fringe(beamsize1)
 #plotphiscan(50)
-beamsize2=1
+beamsize2=10
 #plotphiscan(beamsize2)
+#ideal_fringe(beamsize2)
 plt.xlabel("Relative phase between beam and fringe $\\phi$")
 plt.ylabel("Detected signal (a.u.)")
 plt.title("Phase scan: amplitude noise vs phase + amplitude noise")
@@ -187,10 +146,10 @@ plt.legend()
 plt.tight_layout()
 #plt.xlim([np.pi-np.pi/8,np.pi+np.pi/8])
 #plt.ylim([0,15])
-plt.title(f'Detected photons averaged over {num_avg} shots')
+plt.title(f'Detected photons averaged over {num_avg} shots with {lambda_nm} nm fringe spacing and {dA_noise*100}% laser noise')
 plt.show()
 
-#difference
+#plot difference
 """
 mean_both1, std_both1, mean_A1, std_A1, mean_p1, std_p1=phiscan(beamsize1)
 mean_both2, std_both2, mean_A2, std_A2,mean_p2, std_p2=phiscan(beamsize2)
